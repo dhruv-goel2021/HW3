bf2d891ffa972e09f8e9d1da2bbd50b6d5e1b6de
Dhruv Goel <dhruv.goel@kellogg.northwestern.edu>
2021-04-14 12:49:31 -0500

    updated final sentence

--- noober.js
+++ noober.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', async function() {
   let PassengerFirstName =ride.passengerDetails.first
   let PassengerLastName =ride.passengerDetails.last
   let PassengerPhone = ride.passengerDetails.phoneNumber
-  console.log(`The passenger's name is ${PassengerFirstName} ${PassengerLastName} and their contact information is ${PassengerPhone}`)
+  //practice --> console.log(`The passenger's name is ${PassengerFirstName} ${PassengerLastName} and their contact information is ${PassengerPhone}`)
 
   //Figure out the passenger's pickup location
 let pickupAddress = ride.pickupLocation.address 
@@ -25,7 +25,7 @@ let pickupCity = ride.pickupLocation.city
 let pickupState = ride.pickupLocation.state 
 let pickupZip = ride.pickupLocation.zip 
 
-console.log(`Their pickup locattion is ${pickupAddress}, ${pickupCity}, ${pickupState}, ${pickupZip}`)
+//practice -->console.log(`Their pickup locattion is ${pickupAddress}, ${pickupCity}, ${pickupState}, ${pickupZip}`)
 
   //Figure out the passenger's dropoff location
 let dropoffAddress = ride.dropoffLocation.address
@@ -33,21 +33,26 @@ let dropoffCity = ride.dropoffLocation.city
 let dropoffState = ride.dropoffLocation.state
 let dropoffZip = ride.dropoffLocation.zip 
 
-console.log(`Their dropoff location is ${dropoffAddress}, ${dropoffCity}, ${dropoffState}, ${dropoffZip}`)
+//practice -->console.log(`Their dropoff location is ${dropoffAddress}, ${dropoffCity}, ${dropoffState}, ${dropoffZip}`)
+
 
   //Figure out if the passenger's level of service (Noober X, Noober XL, or Noober Purple)
 let passengerNumber = ride.numberOfPassengers
 let purpleride = ride.purpleRequested
 
+let servicelevel = [`Noober Purple`,`Noober XL`, `Noober X` ]
 if (purpleride == true) {
-  console.log(`Service Level: Noober Purple`)
+  servicelevel = `Noober Purple`
 } else if(passengerNumber >= 4 ) {
-  console.log(`Service Level: Noober XL`)
+  servicelevel = `Noober XL`
 } else {
-  console.log(`Service Level: Noober X`)
+  servicelevel = `Noober X`
 }
-  
 
-//Combine your recipe into one large sentence
-  // 🔥 YOUR CODE ENDS HERE 🔥
+let sentence = `The customer has requested a ${servicelevel} for a total of ${passengerNumber} passengers. The requesting passenger's name is ${PassengerFirstName} ${PassengerLastName} and their contact information is ${PassengerPhone}. Their pickup location is ${pickupAddress}, ${pickupCity}, ${pickupState}, ${pickupZip}. Their dropoff location is ${dropoffAddress}, ${dropoffCity}, ${dropoffState}, ${dropoffZip}.`
+
+// Write your result in one large sentence here
+console.log(sentence)
+
+// 🔥 YOUR CODE ENDS HERE 🔥
 })
